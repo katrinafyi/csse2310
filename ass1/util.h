2e75389ae2141fedf18bd4c2b152d94333a212fc
Kenton Lam <kenton_lam@outlook.com>
2019-08-02 10:41:28 +0000

    cleaned up debug macros
    git-svn-id: https://source.eait.uq.edu.au/svn/csse2310-s4529458/trunk@22 9e73b5d8-9259-024f-850f-dc86fce8e79b

--- ass1/util.h
+++ ass1/util.h
@@ -6,14 +6,16 @@
 #include <stdbool.h>
 
 #ifdef DEBUG
-#define DEBUG_FLAG 1
+#define DEBUG_PRINT(str) fprintf(stderr, "%s:%d %s\n", __func__, __LINE__, \
+        str);
+#define DEBUG_PRINTF(fmt, ...) fprintf(stderr, \
+        "%s:%d "fmt, __func__, __LINE__, __VA_ARGS__);
+
 #else
-#define DEBUG_FLAG 0
+#define DEBUG_PRINT(str) do {} while (0)
+#define DEBUG_PRINTF(fmt, ...) do {} while (0)
 #endif
 
-#define DEBUG_PRINT(str) if (DEBUG_FLAG) fprintf(stderr, "debug: "str"\n");
-#define DEBUG_PRINTF(...) \
-    if (DEBUG_FLAG) fprintf(stderr, "debugf: "__VA_ARGS__);
 
 int parse_int(char* str);
 bool safe_read_line(FILE* file, char** output);
