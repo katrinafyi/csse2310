c3e0a65562c5de54d205c21dc3a1235cdd0da963
Kenton Lam <kenton_lam@outlook.com>
2019-08-13 12:51:48 +0000

    add new test files
    git-svn-id: https://source.eait.uq.edu.au/svn/csse2310-s4529458/trunk@65 9e73b5d8-9259-024f-850f-dc86fce8e79b

---
+++ ass1/_tests/testa1.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]
+then
+    args=test
+else
+    args=$@
+fi
+
+if [ -d .git ]
+then
+    echo "It appears that you are trying to use git for this project."
+    echo "We do not encourage this."
+    exit 1
+fi
+
+if [ -d .hg ]
+then
+    echo "While your independence of thought in not following the git"
+    echo "herd is appreciated, you know very well that we are not"
+    echo "using hg for this project."
+    exit 1
+fi
+
+if [ -d .jazz* ]
+then
+    echo -n "This behaviour is not rational..."
+    sleep 1
+    echo "here is a train"
+    sleep 1
+    sl
+    exit 1
+fi
+
+if [ -d CVS ]
+then
+    echo "No! Just no."
+    exit 1
+fi
+
+svn info >/dev/null 2>/dev/null
+sstat=$?
+if [ $sstat != 0 ]
+then
+    echo "It looks like you might not be running this from an svn working"
+    echo "directory. Since the use of subversion is both assessable in "
+    echo "the course and necessary for assignment submission, we suggest"
+    echo "fixing the subversion issue first."
+    exit 1
+fi
+
+d="$(dirname $0)"; LD_LIBRARY_PATH="$d/lib:$LD_LIBRARY_PATH" PYTHONPATH="$d/lib:$PYTHONPATH" "$d"/tests/grum.py $args
+
