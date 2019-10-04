05caa2dbb96faed00500c1753bda4563c86f0fa8
Kenton Lam <kenton_lam@outlook.com>
2019-10-04 11:06:11 +0000

    add material.c
    git-svn-id: https://source.eait.uq.edu.au/svn/csse2310-s4529458/trunk@141 9e73b5d8-9259-024f-850f-dc86fce8e79b

---
+++ ass4/material.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "material.h"
+#include "util.h"
+
+// see header
+void mat_init(Material* material, int quantity, char* name) {
+    material->quantity = quantity;
+    material->name = strdup(name); // malloc's copy of name
+}
+
+// see header
+void mat_destroy(Material* material) {
+    TRY_FREE(material->name);
+}
