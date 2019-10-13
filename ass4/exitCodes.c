fa99b7ccbb23c89e11c9a509331fe18be1f0b48e
Kenton Lam <kenton_lam@outlook.com>
2019-10-13 06:18:50 +0000

    remove static
    git-svn-id: https://source.eait.uq.edu.au/svn/csse2310-s4529458/trunk@168 9e73b5d8-9259-024f-850f-dc86fce8e79b

--- ass4/exitCodes.c
+++ ass4/exitCodes.c
@@ -7,12 +7,12 @@
 // see header
 const char* depot_message(DepotExitCode code) {
     assert(0 <= code && code < NUM_EXIT_CODES);
-    static const char* messages[NUM_EXIT_CODES];
+    const char* messages[NUM_EXIT_CODES];
 
     messages[D_NORMAL] = "";
     messages[D_INCORRECT_ARGS] = "Usage: 2310depot name {goods qty}\n";
     messages[D_INVALID_NAME] = "Invalid name(s)\n";
     messages[D_INVALID_QUANTITY] = "Invalid quantity\n";
-
+    // string literals are always static
     return messages[code];
 }
