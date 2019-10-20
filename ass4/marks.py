afb9fb5ceea5e375814b7e4208b3f910b9effda0
Kenton Lam <kenton_lam@outlook.com>
2019-10-20 01:04:42 +0000

    improvements
    git-svn-id: https://source.eait.uq.edu.au/svn/csse2310-s4529458/trunk@184 9e73b5d8-9259-024f-850f-dc86fce8e79b

--- ass4/marks.py
+++ ass4/marks.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python2
 
+"""
+Shim marks.py for executing grum.py tests independent of moss.
+
+Features:
+ - Wildcard matching of test names (using Bash's *, ?, []).
+ - More verbose output (up to -vvv), including full diffs.
+ - Delay time multiplier to adjust delays by a factor between 0 and 1.
+ - Display of all failure conditions for a test, not just first failure.
+
+"""
+
 # WARNING: grum.py is python 2
 
 from __future__ import print_function, generators, with_statement, generators
@@ -9,6 +20,7 @@ import os
 import sys
 import time
 import logging
+import signal
 
 from pprint import pprint
 import warnings
@@ -21,8 +33,8 @@ import argparse
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
-delay = 1
 
+delay = 1 # delay time coefficient
 
 class Colour:
     HEADER = '\033[95m'
@@ -40,8 +52,9 @@ class Colour:
 
 class TestProcess(object):
     def __init__(self, i, args, stdin):
-        logger.debug('starting process {} with cmd {} and stdin {}'
-                .format(i, args, repr(stdin)))
+        logger.debug('starting process {} with stdin {}'
+                .format(i, repr(stdin)))
+        logger.debug('    cmd: {}'.format(args))
         if stdin:
             stdin = open(stdin, 'r')
         else:
@@ -49,7 +62,7 @@ class TestProcess(object):
         self.i = i
         self.args = args
         self.process = subprocess.Popen(args, stdout=subprocess.PIPE,
-                stdin=stdin, stderr=subprocess.PIPE)
+            stdin=stdin, stderr=subprocess.PIPE)
 
     def send(self, msg):
         logger.debug('writing to stdin of process {}: {}'.format(self.i, repr(msg)))
@@ -91,25 +104,30 @@ class TestProcess(object):
                 logger.info('{} matched on process {} to file {}'
                         .format(fname, self.i, repr(comparison)))
 
+    signal_names = {getattr(signal, s): s for s in dir(signal)
+            if s.startswith('SIG') and not s.startswith('SIG_')}
+
+    @classmethod
+    def _sig_name(cls, code):
+        pcode = code if code >= 0 else -code
+        s = 'signal ' if code < 0 else 'exit code '
+        s += str(pcode)
+        if code < 0 and pcode in cls.signal_names:
+            s += ' ('+cls.signal_names[pcode]+')'
+        return s
+
+
     def expect_exit(self, code):
         ret = self.process.wait()
         if ret != code:
-            logger.error('process {} expected exit code {} but got {}'
-                    .format(self.i, code, ret))
+            logger.error('process {} expected {} but got {}'
+                    .format(self.i, self._sig_name(code), self._sig_name(ret)))
         else:
-            logger.info('process {} exited correctly with code {}'
-                    .format(self.i, code))
+            logger.info('process {} exited correctly with {}'
+                    .format(self.i, self._sig_name(code)))
 
     def assert_signalled(self, sig):
-        ret = self.process.wait()
-        if ret != -sig:
-            ret_type = 'regular exit code' if ret > 0 else 'signal'
-            sif = sig if sig > 0 else -sig
-            warn('process {} expected signal {} but got {} {}'
-                    .format(self.i, sig, ret_type, ret))
-        else:
-            logger.info('process {} exited correctly with signal {}'
-                    .format(self.i, sig))
+        self.expect_exit(-sig)
 
     def kill(self):
         logger.debug('killing process {}'.format(self.i))
@@ -118,8 +136,7 @@ class TestProcess(object):
 
 class TestCase(object):
     def __init__(self):
-        self._i = 0
-        self.prog = './2310depot' # TODO: generalise
+        self._i = -1
 
     def process(self, args, stdin=None):
         self._i += 1
@@ -170,12 +187,18 @@ class CaptureHandler(logging.NullHandler):
     def handle(self, record):
         self.records.append(record)
 
+def nonneg_float(s):
+    x = float(s)
+    if x < 0:
+        raise argparse.ArgumentTypeError('value cannot be negative: ' + s)
+    return x
+
 def parse_args(args):
     parser = argparse.ArgumentParser(description='Test runner for CSSE2310. '
             +'Shim marks.py by Kenton Lam.')
     parser.add_argument('-v', '--verbose', action='count', default=0,
             help='verbosity, can be repeated up to 3 times. once prints diffs, twice prints successes, thrice prints all actions.')
-    parser.add_argument('-d', '--delay', action='store', type=float, default=1,
+    parser.add_argument('-d', '--delay', action='store', type=nonneg_float, default=1,
             help='delay time multiplier.')
     parser.add_argument('test', type=str, nargs='*', default=('*',),
             help='tests to run. can contain Bash-style wildcards. default: all.')
@@ -197,37 +220,60 @@ def main():
     delay = args.delay
     verbose = min(args.verbose, 3)
 
-    display_level = [logging.ERROR, logging.WARNING, logging.INFO,
-            logging.DEBUG][verbose]
     failure_level = logging.ERROR
     diff_level = logging.WARNING
     success_level = logging.INFO
+    display_level = [logging.ERROR, logging.WARNING, logging.INFO,
+            logging.DEBUG][verbose]
+
+    level_names = {
+        failure_level: Colour.WARNING+'failure',
+        success_level: Colour.OKBLUE+'success',
+        diff_level: 'diff',
+        logging.CRITICAL: Colour.WARNING+Colour.REVERSE+'error'
+    }
 
     handler = CaptureHandler()
     logger.setLevel(logging.DEBUG)
     logger.addHandler(handler)
 
     matched = []
+    seen = set()
     for t in args.test:
-        matched.extend(fnmatch.filter(test_names, '*'+t+'*'))
+        new_matches = [x for x in fnmatch.filter(test_names, '*'+t+'*')
+                if x not in seen]
+        matched.extend(new_matches)
+        seen.update(new_matches)
 
-    print('executing', str(len(matched)), 'tests:')
-    print(matched)
+    print('executing', str(len(matched)), 'tests:', matched)
     print()
 
     max_len = max(len(x) for x in matched) if matched else 0
+    num_len = len(str(len(matched)))
 
     passed = 0
     failed = 0
     start = time.time()
     for name in matched:
-        print(' '*40, end='')
-        print(Colour.MAGENTA+'\r[{:>2}/{}]'.format(1+passed+failed, len(matched))+Colour.ENDC,
+        test_num = '[{}/{}]'.format(1+passed+failed, len(matched))
+        test_num = test_num.rjust(2*num_len+3)
+        print(Colour.MAGENTA+test_num+Colour.ENDC,
                 name.ljust(max_len), '...', end=' ')
         del handler.records[:]
+
         cls, fn = tests[name]
+        cls.__marks_options__ = {'working_dir': ''}
+        try:
+            cls.setup_class() # needed to set cls.prog
+        except (NameError, OSError):
+            pass
         c = cls()
-        fn(c)
+
+        try:
+            fn(c)
+        except Exception as e:
+            logger.critical(e.__class__.__name__+': '+str(e))
+
         if any(r for r in handler.records if r.levelno >= failure_level):
             failed += 1
             print(Colour.FAIL+'FAIL'+Colour.ENDC)
@@ -236,13 +282,8 @@ def main():
             print(Colour.OKGREEN+'OK'+Colour.ENDC)
         for i, r in enumerate(handler.records):
             if r.levelno < display_level: continue
-            if r.levelno == failure_level:
-                print(Colour.WARNING+'failure:'+Colour.ENDC, r.msg)
-            elif r.levelno == success_level:
-                print(Colour.OKBLUE+'success:'+Colour.ENDC, r.msg)
-            else:
-                name = r.levelname.lower()
-                if r.levelno == diff_level: name = 'diff'
-                print(name+':', r.msg)
+            name = r.levelname.lower()
+            if r.levelno in level_names: name = level_names[r.levelno]
+            print(name+':'+Colour.ENDC, r.msg)
     print('\nran', passed+failed, 'tests in', round(time.time()-start, 2), 'seconds.',
             'passed:', str(passed)+',', 'failed:', str(failed)+'.')
